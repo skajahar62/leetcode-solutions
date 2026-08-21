515cc92f04f3af8f90d5ecc88287a28cbe72e58d
Sk Ajahar Karim <ajaharkarim62@gmail.com>
2026-08-22 00:31:51 +0530

    Time: 2 ms (97.41%), Space: 44.8 MB (45.45%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        HashSet<Integer>set=new HashSet<>();
+        for(int num:arr){
+            if(set.contains(num*2)){
+                return true;
+            }
+            if(num%2==0 && set.contains(num/2)){
+                return true;
+            }
+            set.add(num); 
+            
+                   }
+                   return false;
+    }
+}
