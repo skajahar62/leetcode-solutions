ac9570e96b98a407db20d3b2e0aefee494cc55e7
Sk Ajahar Karim <ajaharkarim62@gmail.com>
2026-09-06 12:32:30 +0530

    Time: 2 ms (55.05%), Space: 43.6 MB (45.77%) - LeetHub

---
+++ 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] numberOfPairs(int[] nums) {
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        int pairs=0;
+        int leftover=0;
+        for(int freq:map.values()){
+            pairs+=freq/2;
+            leftover+=freq%2;
+        }
+        return new int[]{pairs,leftover};
+    }
+}
