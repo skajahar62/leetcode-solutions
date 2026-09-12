20eeaaa16430e283c0de592fd67eaa3afdcd5f09
Sk Ajahar Karim <ajaharkarim62@gmail.com>
2026-09-12 12:48:16 +0530

    Time: 0 ms (100%), Space: 42.3 MB (16.95%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.java
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.java
@@ -13,16 +13,18 @@ public class Solution extends GuessGame {
         int right=n;
         while(left<=right){
             int mid=left+(right-left)/2;
-            if(guess(mid)==0){
-                return mid;
+           int guessRes=guess(mid);
+           if(guessRes ==0){
+            return mid;
+           }
+           else if(guessRes==-1){
+            right=mid-1;
+           }
+           else{
+            left=mid+1;
+           }
             }
-            else if(guess(mid)<0){
-                right=mid-1;
-            }
-            else{
-                left=mid+1;
-            }
-        }
+        
         return -1;
     }
 }
