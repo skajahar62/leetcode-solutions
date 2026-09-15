22bd219c688093516979112ca99c56db3bd3aa7d
Sk Ajahar Karim <ajaharkarim62@gmail.com>
2026-09-15 10:42:00 +0530

    Time: 6 ms (22.4%), Space: 45.9 MB (71.77%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+
+        int original=x;
+        int reverse=0;
+
+        while(x>0){
+            int rem=x %10;
+            reverse=reverse *10+rem;
+            x=x/10;
+        }
+        return original ==reverse;
+    }
+}
